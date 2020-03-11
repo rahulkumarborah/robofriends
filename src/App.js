6360368a550d7f7502aee35a4fb5cefd56ceba31
Rahul Kumar Borah <rahulkumarborah@gmail.com>
2020-03-11 08:11:41 +0530

    Added searchbox component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './App.css';
 import CardList from './CardList';
 import {robots} from "./robots";
+import Searchbox from "./Searchbox";
 
 function App() {
-  return <CardList robots={robots}/>
+  return (
+    <div className="tc">
+      <h1>RoboFriends</h1>
+      <Searchbox/>
+      <CardList robots={robots}/>
+    </div>
+  );
 }
 
 export default App;
